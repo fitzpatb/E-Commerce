f497681077cb7c7329ba2e4eecf7397170e212a9
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-04-06 17:53:10 -0600

    finished all models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id'
+      }
+    }
     // define columns
   },
   {
