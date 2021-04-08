401172fe45b09c00deb2c2af4a0602593616adb9
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-04-08 13:25:07 -0600

    finished get routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,28 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tagsData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const oneTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    })
+    res.status(200).json(oneTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
